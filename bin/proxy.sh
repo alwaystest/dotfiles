ff603934d24b56a7b7b06406af4feb6508b33e0c
eric <7553247+alwaystest@users.noreply.github.com>
2019-03-18 17:13:22 +0800

    ADD: proxy set npm

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-export ALL_PROXY=http://127.0.0.1:8001
+PROXY=http://127.0.0.1:8001
+export ALL_PROXY=$PROXY
+if test $(which npm)
+then
+    npm config set proxy $PROXY
+    npm config set https-proxy $PROXY
+fi
--- bin/unproxy.sh
+++ bin/unproxy.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 unset ALL_PROXY
+if test $(which npm)
+then
+    npm config delete proxy
+    npm config delete https-proxy
+fi
