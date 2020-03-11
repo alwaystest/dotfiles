19e3963cbeae1f8246b8827504d31c962bc3e48d
eric <7553247+alwaystest@users.noreply.github.com>
2020-03-11 17:08:48 +0800

    ADD: proxy git

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -2,6 +2,11 @@
 
 PROXY=http://127.0.0.1:8001
 export ALL_PROXY=$PROXY
+if test $(which git)
+then
+git config --global http.proxy $PROXY
+fi
+
 if test $(which npm)
 then
     npm config set proxy $PROXY
--- bin/unproxy.sh
+++ bin/unproxy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 unset ALL_PROXY
+if test $(which git)
+then
+    git config --global --unset http.proxy
+fi
+
 if test $(which npm)
 then
     npm config delete proxy
