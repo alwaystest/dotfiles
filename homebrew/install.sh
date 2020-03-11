a350685ff1953b135a68a7f458da316f7eccd5c2
eric <7553247+alwaystest@users.noreply.github.com>
2020-03-11 17:10:28 +0800

    ADD: fira font

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,13 +16,14 @@ install_brew() {
 
 # Install homebrew packages
 install_basic() {
+    brew tap homebrew/cask-fonts
     brew install z rg tmux
     brew upgrade sqlite3
     brew cask install v2rayx iterm2 squirrel alfred vim
 }
 
 install_extra() {
-    brew cask install shadowsocksx-ng macpass google-chrome scrcpy visual-studio-code spectacle
+    brew cask install shadowsocksx-ng macpass google-chrome scrcpy visual-studio-code spectacle font-firacode-nerd-font-mono
 }
 
 case "$1" in
