2c9be19100876f638ac4a52c50c7f727e782d573
eric <7553247+alwaystest@users.noreply.github.com>
2020-08-06 11:18:19 +0800

    MOD: update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,15 +7,13 @@
 #copy configs
 cp idea/.ideavimrc ~/
 cp tmux/.tmux.conf ~/
-#on my zsh
+#oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp zsh/.zshrc ~/
-#spf13
-sh <(curl https://j.mp/spf13-vim3 -L)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vim/.vimrc.local ~/
 cp vim/.vimrc.before.local ~/
-git config --global user.email alwaystest@users.noreply.github.com
+git config --global user.email 7553247+alwaystest@users.noreply.github.com
 git config --global user.name eric
 ./homebrew/install.sh extra
